31b81430e21af8748a52ec0fca5f562bd295611d
TJ <abongsjoel@gmail.com>
2020-12-20 18:37:51 +0100

    Add the language input

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
 
@@ -39,7 +39,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a Color</label>
+                <label className="label">{label}</label>
                 <div 
                     onClick={() => setOpen(!open)} 
                     className={`ui selection dropdown ${open ? 'visible active' : ''}`}
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -22,10 +22,18 @@ const options = [
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
-            <Dropdown 
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={e => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown
+                label="Select a Language" 
                 selected={language} 
                 onSelectedChange={setLanguage} 
                 options={options} 
