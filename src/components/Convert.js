11c2da9fa3887f8f9ee3f3c28c50d48256d34816
TJ <abongsjoel@gmail.com>
2020-12-20 23:05:54 +0100

    Debounce Translation Updates

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,12 +3,23 @@ import axios from 'axios'
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500)
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [text])
 
     useEffect(() => {
         const doTranslation = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -17,7 +28,7 @@ const Convert = ({ language, text }) => {
         }
 
         doTranslation();
-    }, [language, text]);
+    }, [language, debouncedText]);
 
     return (
         <div>
