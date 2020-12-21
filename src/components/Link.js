91605df6abcbab94a449d7e887c7e860322e13f9
TJ <abongsjoel@gmail.com>
2020-12-21 14:07:08 +0100

    Change the URL

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
     const onClick = (event) => {
         event.preventDefault();
+        window.history.pushState({}, '', href);
     };
 
     return <a onClick={onClick} className={className} href={href}>
