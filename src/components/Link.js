4da5ae81bb849e70a0d1aea2fb8324985bc6d7d2
TJ <abongsjoel@gmail.com>
2020-12-21 14:19:22 +0100

    Detect navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,9 @@ const Link = ({ className, href, children }) => {
     const onClick = (event) => {
         event.preventDefault();
         window.history.pushState({}, '', href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
     };
 
     return <a onClick={onClick} className={className} href={href}>
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
 
 const Route = ({ path, children }) => {
+    useEffect(() => {
+        const onLacationChange = () => {
+            console.log('Location Change');
+        };
+
+        window.addEventListener('popstate', onLacationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLacationChange);
+        }
+    }, []);
     return window.location.pathname === path
         ? children
         : null;
