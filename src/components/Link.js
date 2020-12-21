b8dd289c91dd07c490bd6e04461f5ccb9bcbb780
TJ <abongsjoel@gmail.com>
2020-12-21 14:35:37 +0100

    Handle command clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
     const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+        
         event.preventDefault();
         window.history.pushState({}, '', href);
 
